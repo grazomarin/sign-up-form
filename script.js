a8c8249c2381b33b07240ef9498538f27c3a0f6a
grazomarin <huseynovk355@gmail.com>
2022-07-11 14:09:15 +0400

    added proper password validation, added password comparison verification

--- script.js
+++ script.js
@@ -5,29 +5,38 @@ const master = document.querySelector('.company')
 let companyWidth = Array.from(getComputedStyle(company).width)
 let companyWidthNumb = +companyWidth.slice(0, companyWidth.length-3).join('')
 let masterOffset = +master.getBoundingClientRect().left
-fake_bg.style.width = `${companyWidthNumb+masterOffset}px`
-
+if (companyWidthNumb === 30){
+    fake_bg.style.width = `${companyWidthNumb+masterOffset+270}px`
+} else {
+    fake_bg.style.width = `${companyWidthNumb+masterOffset}px`
+}
 
 window.addEventListener('resize', () => {
     companyWidth = Array.from(getComputedStyle(company).width)
     companyWidthNumb = +companyWidth.slice(0, companyWidth.length-3).join('')
     masterOffset = +master.getBoundingClientRect().left
-    fake_bg.style.width = `${companyWidthNumb+masterOffset}px`
+    if (companyWidthNumb === 30){
+        fake_bg.style.width = `${companyWidthNumb+masterOffset+270}px`
+    } else {
+        fake_bg.style.width = `${companyWidthNumb+masterOffset}px`
+    }
 })
 
-/* let pswrd = document.getElementById('passwd')
+let pswrd = document.getElementById('passwd')
 let pswrdConf = document.getElementById('conf_passwd')
+const submit = document.querySelector('button')
+const msg = document.querySelector('.notmatching')
 
-pswrdConf.addEventListener('focus', () => {
+
+submit.addEventListener('click', () => {
     pswrd = document.getElementById('passwd')
     pswrdConf = document.getElementById('conf_passwd')
 
-    if (pswrd.textContent === pswrdConf.textContent) {
-        console.log(true)
-        console.log(pswrdConf.textContent)
+    if (pswrd.value === pswrdConf.value) {
+        msg.style.visibility = 'hidden';
+        submit.disabled = false;
     } else {
-        console.log(false)
-        console.log(pswrdConf.textContent)
+        msg.style.visibility = 'visible';
+        submit.disabled = true;
     }
 })
- */
